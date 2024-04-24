96f7e591d4b82fc8707991472d8569ba715382f5
BootickSt <e.pyzhyanov@g.nsu.ru>
2024-04-24 18:30:03 +0700

    new

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/egor/upp/Upprpo
-# Build directory: /home/egor/upp/Upprpo/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("libraries/googletest")
-subdirs("tests")
--- build/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/egor/upp/Upprpo/tests
-# Build directory: /home/egor/upp/Upprpo/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/egor/upp/Upprpo/build/tests/hello_test2[1]_include.cmake")
