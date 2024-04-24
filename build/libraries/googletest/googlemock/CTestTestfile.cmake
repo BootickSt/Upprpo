1f454bf817a8efb9d4f2a59da4e8578733d5e4a9
BootickSt <e.pyzhyanov@g.nsu.ru>
2024-04-24 17:26:23 +0700

    project

---
+++ build/libraries/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/egor/upp/Upprpo/libraries/googletest/googlemock
+# Build directory: /home/egor/upp/Upprpo/build/libraries/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
---
+++ build/libraries/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/egor/upp/Upprpo/libraries/googletest/googletest
+# Build directory: /home/egor/upp/Upprpo/build/libraries/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
